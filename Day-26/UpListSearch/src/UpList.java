37b5c37deb98ca6abd341fd68f312a0e390d5ced
nithinkalyan41 <nithinnittu5@gmail.com>
2023-09-08 20:58:43 +0530

    searchmethod

---
+++ Day-26/UpListSearch/src/UpList.java
@@ -0,0 +1,86 @@
+public class UpList {
+    private class Node {
+        Node up;
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+        }
+    }
+    Node head;
+   void addNext(int value){
+       var node = new Node(value);
+       if(head == null)
+           head = node;
+       else {
+           var current = head;
+           while (current.next != null)
+               current = current.next;
+           current.next = node;
+       }
+   }
+    void addUp(int value){
+        var node = new Node(value);
+        if(head == null)
+            head = node;
+        else {
+            var current = head;
+            while (current.up != null)
+                current = current.up;
+            current.up = node;
+        }
+    }
+    void add(int data){
+       if(head == null){
+           head = new Node(data);
+           return;
+       }
+       var current = head;
+       while (current != null){
+           if(current.data > data){
+               if(current.up == null){
+                   current.up = new Node(data);
+                   return;
+               }
+               current = current.up;
+           }else{
+               if(current.next == null){
+                   current.next = new Node(data);
+                   return;
+               }
+               current = current.next;
+           }
+       }
+    }
+    void printNext(){
+       var current = head;
+       while(current != null){
+           System.out.print(current.data+" ");
+           current = current.next;
+       }
+    }
+    void printUp(){
+        var current = head;
+        while(current != null){
+            System.out.print(current.data+" ");
+            current = current.up;
+        }
+    }
+    String search(int data){
+       if(head == null){
+           head = new Node(data);
+       }
+       var current = head;
+       while(current != null){
+           if(current.data == data)
+               return "ElementFound";
+           if(data < current.data){
+               current = current.up;
+           }else{
+               current = current.next;
+           }
+       }
+       return "Element not exist";
+    }
+
+}
