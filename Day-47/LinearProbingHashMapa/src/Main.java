c3391097eaba81fcfdc1e0db0fc7fdfa1b25ad77
nithinkalyan41 <nithinnittu5@gmail.com>
2023-10-22 23:00:46 +0530

    add

---
+++ Day-47/LinearProbingHashMapa/src/Main.java
@@ -0,0 +1,65 @@
+
+import java.util.NoSuchElementException;
+public class Main {
+    public static void main(String[] args) {
+        HashMap h = new HashMap(10);
+        h.put(10,"bruce");
+        h.put(20,"jack");
+        h.put(10,"natasha");
+        h.remove(20);
+        h.put(20,"jack");
+        h.print();
+
+    }
+}
+class HashMap {
+    private class Entry {
+        int key;
+        String value;
+
+        public Entry(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    int size;
+    Entry[] entries;
+
+    public HashMap(int size) {
+        this.size = size;
+        entries = new Entry[size];
+    }
+
+    private int linearProbingHash(int key) {
+        int index = key % size;
+        while (entries[index] != null) {
+            if (entries[index].key == key) {
+                return index;
+            }
+            index = (index + 1) % size;
+        }
+        return index;
+    }
+
+    public void put(int key, String value) {
+        int index = linearProbingHash(key);
+        entries[index] = new Entry(key, value);
+    }
+    public void remove(int key){
+        int index = linearProbingHash(key);
+        if(entries[index].key==0){
+            throw  new NoSuchElementException();
+        }else{
+            entries[index]= new Entry(0,null);
+        }
+    }
+
+    void print(){
+        for(var entry : entries){
+            if(entry != null && entry.value != null) {
+                System.out.println(entry.key + "=" + entry.value);
+            }
+        }
+    }
+}
