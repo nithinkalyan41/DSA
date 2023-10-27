e4cdaccef53ddabe48054081a65421af18c4d063
nithinkalyan41 <nithinnittu5@gmail.com>
2023-10-27 22:36:33 +0530

    add

---
+++ Day-48/twosum.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+public class twosum{
+   public static void main(String args[]){
+   	int a[] = {1,2,3,4,5,6,8,4,5};
+   	int target = 5;
+    HashMap<Integer,Integer> m = new HashMap<>();
+    for (int i = 0; i < a.length; i++) {
+        m.put(a[i], i);
+    }
+    for (int i = 0; i < a.length; i++) {
+        int t = target - a[i];
+        if (m.containsKey(t) && m.get(t) != i) {
+            System.out.println(i+" "+m.get(t));
+            break;
+        }
+    }
+}
+}    
---
+++ Day-49/HashProblems/src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       int a[] = new int[n];
+       int b[] = new int[n];
+       for(int i=0;i<n;i++){
+           a[i]=sc.nextInt();
+       }
+       for(int i=0;i<n;i++){
+           b[i]=sc.nextInt();
+       }
+        System.out.println("O(n) Time and Space Complexity : " + duplicateContains(a,b,n));
+
+        System.out.println("O(nlogn) Time and Space Complexity : " + byUsingBinarySearch(a,b,n));
+    }
+    static boolean duplicateContains(int a[],int b[],int n){
+        Set<Integer> s = new HashSet<>();
+        for(int x : b){
+            s.add(x);
+        }
+        for(int x : a){
+            if(s.contains(x))
+                return true;
+        }
+        return false;
+    }
+    static boolean byUsingBinarySearch(int a[],int b[],int n){
+        Arrays.sort(b);
+        for(int i=0;i<n;i++){
+            int start = 0;
+            int end = n-1;
+            while(start <= end){
+                int mid = (start+end)/2;
+                if(b[mid] == a[i])
+                    return true;
+                else if(b[mid] > a[i])
+                    end = mid-1;
+                else
+                    start = mid+1;
+            }
+        }
+       return false;
+    }
+
+}
