a7042f23135d631f6fd72b4d1f1e43f60603f222
nithinkalyan41 <nithinnittu5@gmail.com>
2024-01-14 23:27:48 +0530

    add

--- Day-67/ParentBit.java
+++ Day-67/ParentBit.java
@@ -18,5 +18,16 @@ public class ParentBit {
         return ")";
       }
   }
+  public int strDist(String str, String sub) {
+    if(str.length()<sub.length())
+        return 0;
+    if(str.substring(0,sub.length()).equals(sub) && str.substring(str.length()-sub.length()).equals(sub)){
+       return str.length();
+    }
+       
+    return strDist(str.substring(0, str.length()-1), sub);    
+  }
+  
+  
   
 }
