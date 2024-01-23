6057a192146edebfdf12b4905856d8d09542e94b
nithinkalyan41 <nithinnittu5@gmail.com>
2024-01-23 22:54:51 +0530

    add

---
+++ leetcode/RotateArray.java
@@ -0,0 +1,19 @@
+class RotataArray{
+	 public void rotate(int[] nums, int k) {
+        int n = nums.length;
+         k %= n;
+         if(k<=0){
+             return;
+         }
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+    }
+    static void reverse(int[] nums,int s,int e){
+        while(s<e){
+            int temp = nums[s];
+            nums[s++]=nums[e];
+            nums[e--]=temp;
+        }
+    }
+}
