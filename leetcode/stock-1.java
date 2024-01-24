b58396c9df635f1b0231d8a9a01fdadf6f46f474
nithinkalyan41 <nithinnittu5@gmail.com>
2024-01-24 23:26:33 +0530

    add

---
+++ leetcode/stock-1.java
@@ -0,0 +1,11 @@
+class stock-1 {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i=0;i<prices.length;i++){
+            min = Math.min(min,prices[i]);
+            profit = Math.max(profit,prices[i]-min);
+        }
+        return profit;
+    }
+}
